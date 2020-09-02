eae4fa587aa797a50cd7619947dbc2360d6f012f
Richard Adams <ra22597@gmail.com>
2020-09-02 23:48:11 +0100

    support grid containers

--- rspace/containers.go
+++ rspace/containers.go
@@ -10,8 +10,14 @@ type ContainerPost struct {
 	Name            string          `yaml:"name" json:"name"`
 	Containers      []ContainerPost `yaml:"containers"`
 	ParentContainer *ParentRef      `json:"parentContainer,omitempty"`
+	GridLayout      *Grid           `json:"gridLayout,omitempty"`
+	Rows            int             `yaml:"rows"`
+	Columns         int             `yaml:"columns"`
 }
-
+type Grid struct {
+	RowsNumber    int `json:"rowsNumber"`
+	ColumnsNumber int `json:"columnsNumber"`
+}
 type ParentRef struct {
 	Id int `json:"id"`
 }
@@ -37,6 +43,11 @@ func (is *InventoryService) CreateContainers(toCreate *ContainerPost) {
 
 func (is *InventoryService) createContainer(toCreate *ContainerPost) (*Container, error) {
 	url := is.containerUrl()
+	if toCreate.Rows > 0 && toCreate.Columns > 0 {
+		toCreate.GridLayout = &Grid{toCreate.Rows, toCreate.Columns}
+	} else {
+		toCreate.GridLayout = nil
+	}
 	data, err := is.doPostJsonBody(toCreate, url)
 	if err != nil {
 		fmt.Println(err.Error())
